aa89421a94a8eea334ef6772964b3d8839cd8c3f
Aquib Ahmed <aquibahmed21@gmail.com>
2022-01-23 23:37:11 +0530

    Database Connections Added (need env file)

---
+++ config/db.js
@@ -0,0 +1,19 @@
+"use strict";
+
+require( 'dotenv' ).config();
+const mongoose = require( "mongoose" );
+const db = process.env.DB_Connection_String;
+
+const connectDB = async () =>
+{
+	try {
+		await mongoose.connect( db, { useNewUrlParser: true } );
+		console.log( "mongoDB Connected..." );
+	} catch (error) {
+		console.log( error );
+		// Exit process with failure
+		process.exit( 1 );
+	}
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 
 require( 'dotenv' ).config();
 const express = require( "express" );
+const connectDB = require( "./config/db" );
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connect Database;
+connectDB();
 
 app.get( '/', ( req, res ) =>
 {
