74980943a25ecdaa28a252deea89bb04e849952d
Aquib Ahmed <aquibahmed21@gmail.com>
2022-01-25 18:58:50 +0530

    Back-end registeration added

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,12 +1,63 @@
-const express = require( "express" );
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const bcrycpt = require("bcryptjs");
+
+const User = require("../../models/Users");
 
 // @route 		GET api/users
-// @desc 			Test router
+// @desc 			Register User
 // @access 		Public
-router.get( "/", ( req, res ) =>
-{
-	res.send( "User route" );
-} );
+router.get("/", (req, res) => {
+  res.send("User route");
+});
+
+router.post(
+  "/",
+  [
+    check("name", "Name is required!").not().isEmpty(),
+    check("email", "Please input valid email").isEmail(),
+    check(
+      "password",
+      "Input valid password with minimum 6 characters"
+    ).isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (user)
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+
+      console.log(1);
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+      console.log(user);
+      console.log(2);
+
+      // Encrypt password
+      const salt = await bcrycpt.genSalt(10);
+      user.password = await bcrycpt.hash(password, salt);
+      await user.save();
+      return res.status(200).send("User Registered");
+
+      // return jsonwebtoken
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,13 +10,19 @@ const PORT = process.env.PORT || 5000;
 // Connect Database;
 connectDB();
 
+// Init Middleware
+app.use( express.json() );
+app.use( express.urlencoded( { extended: false } ) );
+
 // Define Routes
 app.use( "/api/users", require( "./routes/api/user" ) );
 app.use( "/api/auth", require( "./routes/api/auth" ) );
+
 
 app.get( '/', ( req, res ) =>
 {
 	res.status( 200 ).send("API running");
 } );
+
 
 app.listen(PORT, () => console.log("listening to " + PORT));
