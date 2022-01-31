821db9a39cba4dab19b413cbb167c044d8bfa502
Aquib Ahmed <aquibahmed21@gmail.com>
2022-01-31 22:00:03 +0530

    all Orders code updated

--- routes/api/allOrders.js
+++ routes/api/allOrders.js
@@ -5,8 +5,6 @@ const fetch = require( "node-fetch" );
 const CryptoJS = require( "crypto-js" );
 const router = express.Router();
 
-const delay = ( ms ) => new Promise( ( res ) => setTimeout( res, ms ) );
-
 const API = {
 	TICKER: "/sapi/v1/tickers/24hr",
 	ALLORDERBOOK: "/sapi/v1/allOrders",
@@ -25,38 +23,33 @@ const signature = ( queryData, secret ) => CryptoJS.HmacSHA256( queryData, secre
 router.get( "/", async ( req, res ) =>
 {
 	let symbol = "",
-			queryData = "",
-			rawResponse = null,
-			content = null;
+		queryData = "",
+		rawResponse = null,
+		content = null;
 
 	const arr = [];
 	const arrHistory = [];
 	const baseURL = process.env.BASE_URL;
-	const timer = 2100;
 
 	let burl = baseURL + API.TICKER;
 
-	try
-	{
+	try {
 		rawResponse = await fetch( burl,
-		{
-			method: Method.GET,
-			headers: {
-				// 'Accept': 'application/json',
-				// 'Content-Type': 'application/json',
-				'X-Api-Key': process.env.API_KEY,
-			},
+			{
+				method: Method.GET,
+				headers: {
+					// 'Accept': 'application/json',
+					// 'Content-Type': 'application/json',
+					'X-Api-Key': process.env.API_KEY,
+				},
 			} );
 		content = await rawResponse.json();
 		content.forEach( e => arr.push( e.symbol ) );
 
-		for ( symbol of arr )
-		{
-			console.log( { symbol } );
-			await delay( timer );
+		for ( symbol of arr ) {
 			queryData = `symbol=${ symbol }&recvWindow=20000&timestamp=` + ( new Date().getTime() );
 			burl = baseURL + API.ALLORDERBOOK + "?" + queryData +
-						 "&signature=" + signature( queryData, process.env.SECRET_KEY );
+				"&signature=" + signature( queryData, process.env.SECRET_KEY );
 
 			rawResponse = await fetch( burl,
 				{
@@ -66,16 +59,17 @@ router.get( "/", async ( req, res ) =>
 						// 'Content-Type': 'application/json',
 						'X-Api-Key': process.env.API_KEY,
 					},
-				});
+				} );
 
 			content = await rawResponse.json();
-			if ( content )
+
+			if ( content && content.length )
 				arrHistory.push( content );
 		}
 
-		res.status( 200 ).json(arrHistory );
+		res.status( 200 ).json( arrHistory );
 
-	} catch (error) {
+	} catch ( error ) {
 		res.status( 500 ).json( error );
 	}
 } );
