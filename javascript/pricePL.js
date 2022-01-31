94a2dff822df0eeead66a6066b11a14cd6bd9e2a
Aquib Ahmed <aquibahmed21@gmail.com>
2022-01-31 22:31:01 +0530

    buy/sell orders can be executed (code commented)

--- javascript/pricePL.js
+++ javascript/pricePL.js
@@ -17,7 +17,7 @@ const Type = {
 const Route = {
   ORDER: "",
   FUNDS: ""
-}
+};
 
 const arr = [];
 
@@ -33,11 +33,12 @@ const coinDetailsPopup = document.getElementsByClassName( "signup-container" )[ 
 
 const arrTicker = await Const.getTicker() || await Const.getTicker();
 
-document.addEventListener('long-press', async function(e) {
+document.addEventListener( 'long-press', async function ( e )
+{
 
   // https://github.com/john-doherty/long-press-event
   // stop the event from bubbling up
-  e.preventDefault()
+  e.preventDefault();
 
   const targetRow = e.target.tagName == "TD" ? e.target.parentElement : e.target;
 
@@ -45,22 +46,22 @@ document.addEventListener('long-press', async function(e) {
   if ( targetRow.tagName !== "TR" )
     return;
 
-  const coinpair = targetRow.children[0].textContent.trim();
+  const coinpair = targetRow.children[ 0 ].textContent.trim();
   const side = Side.SELL; //Enum
-  const qty = targetRow.children[2].textContent;
+  const qty = targetRow.children[ 2 ].textContent;
   const currentPrice = targetRow.children[ 5 ].textContent.split( " " )[ 0 ];
   const type = Type.LIMIT; //Enum
 
   // try
   // {
-  //   const rawResponse = await fetch( '/order/test',
+  //   const rawResponse = await fetch( '/api/order',
   //   {
   //     method: 'POST',
   //     headers: {
   //       'Accept': 'application/json',
   //       'Content-Type': 'application/json'
   //     },
-  //     body: JSON.stringify( { coinpair, side, qty, currentPrice, type } )
+  //     body: JSON.stringify( { coinpair, side, qty, currentPrice, type, side } )
   //   });
 
   //   const content = await rawResponse.json();
@@ -69,16 +70,16 @@ document.addEventListener('long-press', async function(e) {
   // } catch (error) {
   //   console.log(error);
   // }
-});
+} );
 
-window.onanimationend = async (e) => {
+window.onanimationend = async ( e ) =>
+{
   // stacksnippet's console also has CSS animations...
-  if ( e.type === "animationend" && e.target.id == notificationBTN.id )
-  {
+  if ( e.type === "animationend" && e.target.id == notificationBTN.id ) {
     await Const.delay( 11000 );
     notificationBTN.classList.remove( "visible" );
   }
-}
+};
 
 coinDetailsPopup.addEventListener( "click", async ( event ) =>
 {
@@ -91,44 +92,43 @@ coinDetailsPopup.addEventListener( "click", async ( event ) =>
     }
       break;
     case "next":
-    {
-      const coin = coinDetailsPopup.querySelector( "#pets-name" ).value;
-      const pair = coinDetailsPopup.querySelector( "[checked]" )?.value || "usdt";
-      const qty = +coinDetailsPopup.querySelector( "#pets-breed" ).value;
-      const price = +coinDetailsPopup.querySelector( "#pets-birthday" ).value;
-      const term = coinDetailsPopup.querySelector( "#divCommentTerm" ).textContent;
-
-      if ( !coin || !pair || !qty || !price ) {
-        notificationBTN.innerHTML = `<p>Please fill all the fields</p>`;
-        notificationBTN.classList.add( "visible" );
-        return;
-      }
-      else {
-        coinDetailsPopup.classList.add( "Util_hide" );
-        ResetForm();
-      }
-
-      const jsonStringyfied = { coin, pair, qty, price, term };
-
-      try
       {
-        const rawResponse = await fetch( '/api/hodling',
-        {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(jsonStringyfied)
-        });
+        const coin = coinDetailsPopup.querySelector( "#pets-name" ).value;
+        const pair = coinDetailsPopup.querySelector( "[checked]" )?.value || "usdt";
+        const qty = +coinDetailsPopup.querySelector( "#pets-breed" ).value;
+        const price = +coinDetailsPopup.querySelector( "#pets-birthday" ).value;
+        const term = coinDetailsPopup.querySelector( "#divCommentTerm" ).textContent;
 
-        const content = await rawResponse.json();
-        console.log(content);
+        if ( !coin || !pair || !qty || !price ) {
+          notificationBTN.innerHTML = `<p>Please fill all the fields</p>`;
+          notificationBTN.classList.add( "visible" );
+          return;
+        }
+        else {
+          coinDetailsPopup.classList.add( "Util_hide" );
+          ResetForm();
+        }
 
-      } catch (error) {
-        console.log(error);
+        const jsonStringyfied = { coin, pair, qty, price, term };
+
+        try {
+          const rawResponse = await fetch( '/api/hodling',
+            {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify( jsonStringyfied )
+            } );
+
+          const content = await rawResponse.json();
+          console.log( content );
+
+        } catch ( error ) {
+          console.log( error );
+        }
       }
-    }
   }
 
   function ResetForm ()
@@ -156,8 +156,8 @@ let usdtinr = arrTicker.filter( e => e.symbol == ( "usdtinr" ) )[ 0 ].lastPrice;
 
 await AddHodlingRows_FromJSON( Const.JSONDATA );
 await AddPLRows_FromJSON( Const.SoldJSon );
-SummationPLTable(hodlingBody);
-SummationPLTable(plBody);
+SummationPLTable( hodlingBody );
+SummationPLTable( plBody );
 webSocket.wsSubscribe( LiveUpdateHodlingTable );
 
 hodlingBody[ 0 ].addEventListener( "click", OnClick_HodlingRows() );
@@ -182,19 +182,19 @@ function OnDBClick_HodlingRows ()
     };
 
     const getStopLossValue = ( price, percentage, decimal = 1 ) =>
-      (price - ((price * percentage)/100)).toFixed(decimal)
+      ( price - ( ( price * percentage ) / 100 ) ).toFixed( decimal );
 
     const buyPrice = +targetRow.children[ 1 ].textContent;
     const currentPrice = +targetRow.children[ 5 ].textContent.split( " " )[ 0 ];
     const countNumber = +countDecimals( buyPrice );
-    const margin = +targetRow.children[10].textContent.split( " " )[ 0 ];
+    const margin = +targetRow.children[ 10 ].textContent.split( " " )[ 0 ];
 
     notificationBTN.innerHTML = `<p>Trigger Value at ${ getStopLossValue( currentPrice, 2, countNumber ) } <br />
     Sale Value at ${ getStopLossValue( currentPrice, 4, countNumber ) } <br />
-    You get ${ getStopLossValue(margin, 4, countNumber) } ₹</p>`;
+    You get ${ getStopLossValue( margin, 4, countNumber ) } ₹</p>`;
 
     notificationBTN.classList.add( "visible" );
-  }
+  };
 }
 
 function OnClick_HodlingRows ()
@@ -208,8 +208,7 @@ function OnClick_HodlingRows ()
       return;
 
     const existingRow = hodlingBody[ 0 ].querySelector( ".rowSelect" );
-    if ( existingRow )
-    {
+    if ( existingRow ) {
       if ( existingRow.id == targetID )
         existingRow.classList.remove( "rowSelect" );
       else {
@@ -222,22 +221,21 @@ function OnClick_HodlingRows ()
   };
 }
 
-function SummationPLTable (body)
+function SummationPLTable ( body )
 {
   const children = body[ 0 ].children;
   const footChild = body[ 0 ].nextElementSibling.children[ 0 ].children;
 
   let beforeDollar = 0, beforeInr = 0, percentage = 0, afterDollar = 0, afterInr = 0, portFolio = 0;
 
-  for ( let child of children )
-  {
+  for ( let child of children ) {
     beforeDollar += +child.querySelector( "#tdTotalDollar" ).textContent.split( " " )[ 0 ];
     beforeInr += +child.querySelector( "#tdTotalinr" ).textContent.split( " " )[ 0 ];
     percentage += +child.querySelector( "#tdPLPercentage" ).textContent.split( " " )[ 0 ];
     afterDollar += +child.querySelector( "#tdMarginDol" ).textContent.split( " " )[ 0 ];
     afterInr += +child.querySelector( "#tdMarginINR" ).textContent.split( " " )[ 0 ];
 
-    if (child.querySelector( "#tdCurTotalinr" ))
+    if ( child.querySelector( "#tdCurTotalinr" ) )
       portFolio += +child.querySelector( "#tdCurTotalinr" ).textContent.split( " " )[ 0 ];
   }
 
@@ -247,9 +245,8 @@ function SummationPLTable (body)
   footChild[ 3 ].textContent = "₿ Gain: " + afterDollar.toFixed( 2 );
   footChild[ 4 ].textContent = "₹ Gain: " + afterInr.toFixed( 2 );
 
-  if ( portFolio )
-  {
-    const lastPrice = +footChild[ 5 ].getAttribute( "lastPrice") || 0;
+  if ( portFolio ) {
+    const lastPrice = +footChild[ 5 ].getAttribute( "lastPrice" ) || 0;
     const currentPrice = portFolio.toFixed( 2 );
     const color = lastPrice.toFixed( 2 ) == currentPrice ? "" : ( lastPrice < currentPrice ? "green" : "red" );
     footChild[ 5 ].setAttribute( "lastPrice", currentPrice );
@@ -259,7 +256,7 @@ function SummationPLTable (body)
 }
 
 async function AddHodlingRows_FromJSON ( obj,
-                                         isUpdate = false )
+  isUpdate = false )
 {
   await UpdateFearGreedIndex( usdtinr );
   for ( const key in obj ) {
@@ -272,8 +269,7 @@ async function AddHodlingRows_FromJSON ( obj,
         document.getElementById( "templatePLRow" ).content.cloneNode( true );
 
       const isINR = pair == "inr";
-      if ( !isUpdate )
-      {
+      if ( !isUpdate ) {
         const rowParent = row.getElementById( "tdPairName" ).parentElement;
         rowParent.id = key;
         rowParent.setAttribute( "pairName", coin + pair );
@@ -305,13 +301,11 @@ async function AddHodlingRows_FromJSON ( obj,
       row.querySelector( "#tdMarginDol" ).textContent = marginDollar.toFixed( 2 ) + " ₿";
       row.querySelector( "#tdMarginINR" ).textContent = marginINR.toFixed( 2 ) + " ₹";
 
-      if ( percentage > 0 )
-      {
+      if ( percentage > 0 ) {
         row.querySelector( "#tdMarginINR" ).parentNode.classList = "Profit";
       }
 
-      if ( !isUpdate )
-      {
+      if ( !isUpdate ) {
         if ( percentage > 0 )
           hodlingBody[ 0 ].prepend( row );
         else
@@ -322,13 +316,11 @@ async function AddHodlingRows_FromJSON ( obj,
 }
 
 async function AddPLRows_FromJSON ( obj,
-                                    isUpdate = false )
+  isUpdate = false )
 {
-  for ( const key in obj )
-  {
+  for ( const key in obj ) {
 
-    if ( obj.hasOwnProperty( key ) )
-    {
+    if ( obj.hasOwnProperty( key ) ) {
       const { coin, pair, qty, buyPrice, soldPrice, term } = obj[ key ];
       const row = isUpdate ? document.querySelector( "#" + key ) :
         document.getElementById( "templatePL1Row" ).content.cloneNode( true );
@@ -359,8 +351,7 @@ async function AddPLRows_FromJSON ( obj,
       row.querySelector( "#tdMarginDol" ).textContent = marginDollar.toFixed( 2 ) + " ₿";
       row.querySelector( "#tdMarginINR" ).textContent = marginINR.toFixed( 2 ) + " ₹";
 
-      if ( percentage < 0 )
-      {
+      if ( percentage < 0 ) {
         const parentRow = row.querySelector( "#tdMarginINR" ).parentNode.style;
         parentRow.background = "#c77777";
         parentRow.color = "#fff";
@@ -374,23 +365,19 @@ async function AddPLRows_FromJSON ( obj,
 
 async function LiveUpdateHodlingTable ( event )
 {
-  try
-  {
-    if ( event.event && (event.event == "subscribed" || event.event == "unsubscribed") )
+  try {
+    if ( event.event && ( event.event == "subscribed" || event.event == "unsubscribed" ) )
       return;
 
     const array = event.data.filter( e => arr.includes( e.s ) );
     const usdtINRArray = array.filter( e => e.s == "usdtinr" );
 
-    if ( usdtINRArray.length )
-    {
+    if ( usdtINRArray.length ) {
       usdtinr = usdtINRArray[ 0 ].c;
       await UpdateFearGreedIndex( usdtinr );
     }
-    for ( let arrs of array )
-    {
-      for ( let child of hodlingBody[ 0 ].querySelectorAll( "tr[pairName=" + arrs.s + "]" ) )
-      {
+    for ( let arrs of array ) {
+      for ( let child of hodlingBody[ 0 ].querySelectorAll( "tr[pairName=" + arrs.s + "]" ) ) {
         const lastPrice = +child.querySelector( "#tdCurrentPrice" ).textContent.split( " " )[ 0 ];
         const qty = +child.querySelector( "#tdQty" ).textContent;
         const currentPrice = +arrs.c;
@@ -423,15 +410,13 @@ async function LiveUpdateHodlingTable ( event )
         child.querySelector( "#tdMarginDol" ).style.color = color;
         child.querySelector( "#tdMarginINR" ).style.color = color;
 
-        if ( prevPercentage < 0 && percentage.toFixed(2) > 0 )
-        {
+        if ( prevPercentage < 0 && percentage.toFixed( 2 ) > 0 ) {
           // move row to top
           const parent = child.parentNode;
-          parent.insertBefore(child, child.parentNode.querySelector(".Profit"))
+          parent.insertBefore( child, child.parentNode.querySelector( ".Profit" ) );
           child.classList = "Profit";
         }
-        else if ( prevPercentage > 0 && percentage.toFixed(2) < 0 )
-        {
+        else if ( prevPercentage > 0 && percentage.toFixed( 2 ) < 0 ) {
           const parent = child.parentNode;
           parent.insertBefore( child, [ ...child.parentNode.querySelectorAll( ".Profit" ) ].pop().nextElementSibling );
           child.classList = "";
@@ -450,8 +435,7 @@ async function UpdateFearGreedIndex ( usdtinr, isUpdate = false )
   const divFearGreedContainer = document.getElementsByClassName( "divFearGreedIndex" );
   divFearGreedContainer[ 0 ].querySelector( "#spanDollarPrice" ).textContent = usdtinr;
 
-  if ( !isUpdate )
-  {
+  if ( !isUpdate ) {
     const fearGreed = await Const.fearGreed();
     divFearGreedContainer[ 0 ].querySelector( "#spanValue" ).textContent = fearGreed.data[ 0 ].value + " %";
     divFearGreedContainer[ 0 ].querySelector( "#spanClassification" ).textContent = fearGreed.data[ 0 ].value_classification;
--- routes/api/order.js
+++ routes/api/order.js
@@ -2,45 +2,43 @@ require( 'dotenv' ).config();
 
 const express = require( "express" );
 const fetch = require( "node-fetch" );
+const CryptoJS = require( "crypto-js" );
 const router = express.Router();
 
-// @route 		GET api/funds
+// @route 		GET api/order
 // @desc 			Get
 // @access 		Public
-router.get( "/", async ( req, res ) =>
-{
 
-	// const { coinpair, side, qty, currentPrice, type } = req.body;
-	// res.status( 200 ).json( { coinpair, side, qty, currentPrice, type } );
+router.post( "/", async ( req, res ) =>
+{
+	const { coinpair, side, qty, currentPrice, type } = req.body;
 
 	const baseURL = process.env.BASE_URL;
 	const signature = ( queryData, secret ) => CryptoJS.HmacSHA256( queryData, secret ).toString( CryptoJS.enc.Hex );
-	// const queryData = "symbol=wrxusdt&side=buy&type=limit&price=500&quantity=1&recvWindow=10000&timestamp=" + ( new Date().getTime() );
-	const queryData = "symbol=ftmusdt&side=sell&type=limit&price=2.0874&quantity=1.5&recvWindow=10000&timestamp=" + ( new Date().getTime() );
+	const queryData = `symbol=${coinpair}&side=${side}&type=${type}&price=${currentPrice}&quantity=${qty}&recvWindow=10000&timestamp=` + ( new Date().getTime() );
 
 	const api = "/sapi/v1/order/";
 	const burl = baseURL + api + "?" + queryData + "&signature=" + signature( queryData, process.env.SECRET_KEY );
 
+	// try
+	// {
+	// 	const rawResponse = await fetch( burl,
+	// 	{
+	// 		method: 'POST',
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+	// 			"Content-Type": "application/x-www-form-urlencoded",
+	// 			'X-Api-Key': process.env.API_KEY,
+	// 		},
+	// 	});
 
-	try
-	{
-		const rawResponse = await fetch( burl,
-		{
-			method: 'POST',
-			headers: {
-				"Content-Type": "application/json",
-				"Content-Type": "application/x-www-form-urlencoded",
-				'X-Api-Key': process.env.API_KEY,
-			},
-		});
+	// 	const content = await rawResponse.json();
+	// 	res.status( 200 ).json( content );
 
-		const content = await rawResponse.json();
-		// const fundsHodling = content.filter(e => Number(e.free))
-		res.status( 200 ).json( content );
+	// } catch (error) {
+	// 	res.status( 500 ).json( error );
+	// }
 
-	} catch (error) {
-		res.status( 500 ).json( error );
-	}
 } );
 
 module.exports = router;
