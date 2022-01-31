80bcd501faa96c8bd8062c0764d45c94949ac8a4
Aquib Ahmed <aquibahmed21@gmail.com>
2022-01-31 22:06:45 +0530

    updated funds.js

--- routes/api/funds.js
+++ routes/api/funds.js
@@ -2,37 +2,34 @@ require( 'dotenv' ).config();
 
 const express = require( "express" );
 const fetch = require( "node-fetch" );
+const CryptoJS = require( "crypto-js" );
 const router = express.Router();
+
+const signature = ( queryData, secret ) => CryptoJS.HmacSHA256( queryData, secret ).toString( CryptoJS.enc.Hex );
 
 // @route 		GET api/funds
 // @desc 			Get
 // @access 		Public
 router.get( "/", async ( req, res ) =>
 {
-	const baseURL = process.env.BASE_URL;
-	const signature = ( queryData, secret ) => CryptoJS.HmacSHA256( queryData, secret ).toString( CryptoJS.enc.Hex );
-	const queryData = "symbol=shibusdt&recvWindow=20000&timestamp=" + (new Date().getTime());
-			// const queryData = "recvWindow=20000&timestamp=" + ( new Date().getTime() );
+	const queryData = "recvWindow=20000&timestamp=" + ( new Date().getTime() );
 	const api = "/sapi/v1/funds";
-	const burl = baseURL + api + "?" + queryData + "&signature=" + signature( queryData, process.env.SECRET_KEY );
+	const burl = process.env.BASE_URL + api + "?" + queryData + "&signature=" + signature( queryData, process.env.SECRET_KEY );
 
-	try
-	{
+	try {
 		const rawResponse = await fetch( burl,
-		{
-			method: 'GET',
-			headers: {
-				// 'Accept': 'application/json',
-				// 'Content-Type': 'application/json',
-				'X-Api-Key': process.env.API_KEY,
-			},
-		});
+			{
+				method: "GET",
+				headers: {
+					"X-Api-Key": process.env.API_KEY,
+				},
+			} );
 
 		const content = await rawResponse.json();
-		const fundsHodling = content.filter(e => Number(e.free))
+		const fundsHodling = content.filter( e => Number( e.free ) );
 		res.status( 200 ).json( fundsHodling );
 
-	} catch (error) {
+	} catch ( error ) {
 		res.status( 500 ).json( error );
 	}
 } );
