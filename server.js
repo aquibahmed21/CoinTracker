0ccfe0e0dc9dc37888c2f4d63a7492858c444dbd
Aquib Ahmed <aquibahmed21@gmail.com>
2022-02-17 21:30:20 +0530

    Code updated

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const cors = require( "cors" );
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+console.log( "PORT: ", process.env.PORT );
+
 app.use( cors() );
 app.options( "*", cors() );
 
